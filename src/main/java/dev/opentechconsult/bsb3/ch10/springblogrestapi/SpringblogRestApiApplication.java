02c4d6963343f0b61999edb4096e9b8bdce57706
Elom Atsou AGBOKA <elom.agboka@outlook.com>
2026-02-09 07:16:26 +0000

    Spring Blog REST API initial commit

---
+++ src/main/java/dev/opentechconsult/bsb3/ch10/springblogrestapi/SpringblogRestApiApplication.java
@@ -0,0 +1,13 @@
+package dev.opentechconsult.bsb3.ch10.springblogrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringblogRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringblogRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/opentechconsult/bsb3/ch10/springblogrestapi/SpringblogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.opentechconsult.bsb3.ch10.springblogrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringblogRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
